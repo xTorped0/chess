ef56fa742a30c2f6ed77c083873ad390dc3f6a6d
dabizha.vanya <dabizha.vanya@gmail.com>
2024-05-17 13:21:59 +0300

    chore: Update App component styling
    Update the styling of the App component in the App.tsx file to center the content vertically. This change improves the visual presentation of the application.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Game from './game';
 
 function App() {
 	return (
-		<div className='relative flex flex-1 flex-col min-h-screen overflow-hidden bg-gray-200 py-4 px-5 gap-3'>
+		<div className='relative flex flex-1 flex-col min-h-screen overflow-hidden bg-gray-200 py-4 px-5 gap-3 justify-center '>
 			{/* <Header /> */}
 			<Game />
 		</div>
